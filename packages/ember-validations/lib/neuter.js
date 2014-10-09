13d29291638dd116906d8ccd468d6c4f570be440
Jiangti Wan-Leong <jiangti.wan.leong@gmail.com>
2014-10-09 19:18:45 +1100

    Package for neuter.js

---
+++ packages/ember-validations/lib/neuter.js
@@ -0,0 +1,13 @@
+require("./error");
+require("./errors");
+
+require("./validations");
+
+require("./validators");
+require("./validator");
+
+require("./validators/presence");
+require("./validators/numericality");
+require("./validators/length");
+require("./validators/format");
+require("./validators/match");
