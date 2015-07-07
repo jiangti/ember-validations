7fd9ab091d8821fd45e4ad6b0312ac352e04081f
Jiangti Wan-Leong <jiangti.wan.leong@gmail.com>
2015-07-07 16:06:22 +1000

    Fixing the ember missing get() function?

--- dist/ember-validations.prod.js
+++ dist/ember-validations.prod.js
@@ -1040,7 +1040,7 @@ Ember.Validators.MatchValidator = Ember.Validator.extend({
 Ember.Validators.ReqwhenValidator = Ember.Validator.extend({
 
 	_validate: function(obj, attr, value) {
-		var options = get(this, 'options') || {};
+		var options = Em.get(this, 'options') || {};
 		if (options.property) {
 
 			if (obj.get(options.property) && Em.isEmpty(value)) {
--- packages/ember-validations/lib/validators/reqwhen.js
+++ packages/ember-validations/lib/validators/reqwhen.js
@@ -1,7 +1,7 @@
 Ember.Validators.ReqwhenValidator = Ember.Validator.extend({
 
 	_validate: function(obj, attr, value) {
-		var options = get(this, 'options') || {};
+		var options = Em.get(this, 'options') || {};
 		if (options.property) {
 
 			if (obj.get(options.property) && Em.isEmpty(value)) {
