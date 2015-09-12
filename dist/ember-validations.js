07e611f039217f4bdbaed0c10247d59aafa93850
Jiangti Wan-Leong <jiangti.wan.leong@gmail.com>
2015-09-13 00:36:22 +1000

    Why isnt empty being skipped?

--- dist/ember-validations.js
+++ dist/ember-validations.js
@@ -801,9 +801,9 @@ Ember.Validators.FormatValidator = Ember.Validator.extend({
 var get = Ember.get;
 
 Ember.ValidationError.addMessages({
-  'tooShortLength': "is too short (minimum @{value} characters)",
-  'tooLongLength': "is too long (maximum @{value} characters)",
-  'wrongLength': "is the wrong length (should be @{value} characters)"
+	'tooShortLength': "is too short (minimum @{value} characters)",
+	'tooLongLength': "is too long (maximum @{value} characters)",
+	'wrongLength': "is the wrong length (should be @{value} characters)"
 });
 
 /**
@@ -837,40 +837,52 @@ Ember.ValidationError.addMessages({
 
    @extends Ember.Validator
  */
-Ember.Validators.LengthValidator = Ember.Validator.extend(/** @scope Ember.Validators.LengthValidator */{
+Ember.Validators.LengthValidator = Ember.Validator.extend( /** @scope Ember.Validators.LengthValidator */ {
+	shouldSkipValidations: function(obj, attr, value) {
+		if (Em.isEmpty(value)) {
+			return true;
+		} else {
+			return false;
+		}
+	},
 
-  /** @private */
-  _validate: function(obj, attr, value) {
-    var options = get(this, 'options'),
-        errors = get(obj, 'validationErrors'),
-        length = value ? Ember.get(value, 'length') : 0,
-        optionValue;
+	/** @private */
+	_validate: function(obj, attr, value) {
+		var options = get(this, 'options'),
+			errors = get(obj, 'validationErrors'),
+			length = value ? Ember.get(value, 'length') : 0,
+			optionValue;
 
-    optionValue = this.optionValue(obj, 'is', 'number');
-    if (optionValue === null) {
-      optionValue = this.optionValue(obj, 'value', 'number');
-    }
+		optionValue = this.optionValue(obj, 'is', 'number');
+		if (optionValue === null) {
+			optionValue = this.optionValue(obj, 'value', 'number');
+		}
 
-    if (optionValue !== null) {
-      if (length !== optionValue) {
-        errors.add(attr, 'wrongLength', {value: optionValue});
-      }
-    } else {
+		if (optionValue !== null) {
+			if (length !== optionValue) {
+				errors.add(attr, 'wrongLength', {
+					value: optionValue
+				});
+			}
+		} else {
 
-      optionValue = this.optionValue(obj, 'minimum', 'number');
-      if (optionValue !== null && length < optionValue) {
-        errors.add(attr, 'tooShortLength', {value: optionValue});
-      }
+			optionValue = this.optionValue(obj, 'minimum', 'number');
+			if (optionValue !== null && length < optionValue) {
+				errors.add(attr, 'tooShortLength', {
+					value: optionValue
+				});
+			}
 
-      optionValue = this.optionValue(obj, 'maximum', 'number');
-      if (optionValue !== null && length > optionValue) {
-        errors.add(attr, 'tooLongLength', {value: optionValue});
-      }
-    }
-  }
+			optionValue = this.optionValue(obj, 'maximum', 'number');
+			if (optionValue !== null && length > optionValue) {
+				errors.add(attr, 'tooLongLength', {
+					value: optionValue
+				});
+			}
+		}
+	}
 
 });
-
 })();
 (function() {
 var get = Ember.get;
