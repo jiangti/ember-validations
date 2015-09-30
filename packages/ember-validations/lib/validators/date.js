c411b2481c83d0c9bd6a5e220a28df10a4b60cc0
Jiangti Wan-Leong <jiangti.wan.leong@gmail.com>
2015-10-01 00:54:50 +1000

    Bday

--- packages/ember-validations/lib/validators/date.js
+++ packages/ember-validations/lib/validators/date.js
@@ -1,13 +1,33 @@
 Ember.ValidationError.addMessage('date', "must be a valid date");
+Ember.ValidationError.addMessage('bday', "must be at least @{value} years old");
 
 Ember.Validators.DateValidator = Ember.Validator.extend({
     _validate: function (obj, attr, value) {
 
-        var ok = moment(value, "DD-MM-YYYY").isValid();
+    	var options = get(this, 'options'),
+			errors = get(obj, 'validationErrors'),
+			length = value ? Ember.get(upcast.to(value, 'string'), 'length') : 0,
+			optionValue;
+
+		var m = moment(value, "DD-MM-YYYY");
+
+        var ok = m.isValid();
 
         if (!ok) {
             obj.get('validationErrors').add(attr, "date", '', obj.get('validations.' + attr + '.date.message'));
+        } else {
+
+        	optionValue = this.optionValue(obj, 'age', 'number');
+
+			if (optionValue !== null) {
+				var n = moment().subtract(optionValue, 'years');
+				if (m.isAfter(n ,'day')) {
+					obj.get('validationErrors').add(attr, "bday");
+				}
+			} 
         }
+
+
     }
 });
 
